5cf422ce376b0281d87d2f0feea1e43306abcae5
Zahra Tavakoli <z.tavakoli@ec.iut.ac.ir>
2024-07-31 11:55:51 +0330

    516. Longest Palindromic Subsequence

---
+++ Leetcode/Cpp/516. Longest Palindromic Subsequence.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    // Reduce LPS to LCS using the following lemma:
+    // |LPS(s)|=|LCS(s,reverse(s))|
+    int longestCommonSubsequence(string text1, string text2) {
+        int** A = new int* [text1.length() + 1];
+        for (int i = 0; i < text1.length() + 1; i++)
+            A[i] = new int[text2.length() + 1];
+
+        for (int i = 0; i < text2.length() + 1; i++)
+            A[0][i] = 0;
+        for (int i = 0; i < text1.length() + 1; i++)
+            A[i][0] = 0;
+
+        for (int i = 1; i < text1.length() + 1; i++) {
+            for (int j = 1; j < text2.length() + 1; j++) {
+                if (text1[i - 1] == text2[j - 1])
+                    A[i][j] = 1 + A[i - 1][j - 1];
+                else {
+                    A[i][j] = A[i][j - 1];
+                    if (A[i - 1][j] > A[i][j])
+                        A[i][j] = A[i - 1][j];
+                }
+            }
+        }
+
+        return A[text1.length()][text2.length()];
+    }
+    int longestPalindromeSubseq(string s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        string tmp = s;
+        reverse(s.begin(), s.end());
+        return longestCommonSubsequence(s, tmp);
+    }
+};
